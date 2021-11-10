574f019f094108145381ac86f7b58629770bf65f
Andrei <soprachev@mail.ru>
2021-11-10 04:03:35 +0300

    refresh

--- PolyNavi/Views/TimetableVC/TimetableWeekVC/TimetableViewController.swift
+++ PolyNavi/Views/TimetableVC/TimetableWeekVC/TimetableViewController.swift
@@ -24,6 +24,10 @@ class TimetableViewController: UIViewController {
     //MARK:-Properties
     
     internal var arrayOfDaysWithLessons: [TimetableWeek.TimetableDay] = []
+    internal lazy var refreshControl: UIRefreshControl = {
+        $0.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
+        return $0
+    }(UIRefreshControl())
     
     internal lazy var tableView: UITableView = {
         $0.register(LessonCellView.self, forCellReuseIdentifier: LessonCellView.identifire)
@@ -47,6 +51,23 @@ class TimetableViewController: UIViewController {
         return $0
     }(UIActivityIndicatorView())
     
+    internal lazy var emptyWeek: UIImageView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(systemName: "bed.double", withConfiguration: UIImage.SymbolConfiguration(pointSize: 100))
+        $0.isHidden = true
+        return $0
+    }(UIImageView())
+    
+    internal lazy var emptyWeekDscription: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        $0.font = .preferredFont(forTextStyle: .callout)
+        $0.text = L10n.Timetable.emptyWeek
+        return $0
+    }(UILabel())
+    
+    
     //MARK:- Life cicle
     
     override func viewDidLoad() {
@@ -59,6 +80,16 @@ class TimetableViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
+    
+    @objc
+    func handleRefreshControl() {
+        refreshDate() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+        }
+    }
+    
 }
 
 
@@ -67,6 +98,10 @@ private extension TimetableViewController {
     func layoutViews() {
         view.addSubview(loader)
         view.addSubview(tableView)
+        view.addSubview(emptyWeek)
+        
+        emptyWeek.addSubview(emptyWeekDscription)
+        
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -74,7 +109,15 @@ private extension TimetableViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
             loader.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            loader.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            loader.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            emptyWeek.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyWeek.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            emptyWeekDscription.centerXAnchor.constraint(equalTo: emptyWeek.centerXAnchor),
+            emptyWeekDscription.topAnchor.constraint(equalTo: emptyWeek.bottomAnchor, constant: 10),
+            emptyWeekDscription.widthAnchor.constraint(lessThanOrEqualToConstant: 400),
+            emptyWeekDscription.widthAnchor.constraint(lessThanOrEqualTo: view.widthAnchor, constant: -20)
         ])
     }
 }
@@ -83,8 +126,10 @@ private extension TimetableViewController {
 //MARK:- Api
 extension TimetableViewController {
     public func loadData() {
-        self.arrayOfDaysWithLessons = []
-        self.tableView.reloadData()
+        arrayOfDaysWithLessons = []
+        tableView.reloadData()
+        emptyWeek.isHidden = true
+        tableView.refreshControl = nil
         
         loader.startAnimating()
         
@@ -93,13 +138,36 @@ extension TimetableViewController {
         TimetableProvider.shared.loadTimetabe(id: id ?? -1, filter: GroupsAndTeacherStorage.shared.currentFilter, startDate: date, fromCache: true) {
             response in
             guard let response = response.data else { return }
-            let timetable = TimetableWeek.convert(response)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0) { [weak self] in
-                self?.arrayOfDaysWithLessons = timetable.days.map { pair in
-                    return TimetableWeek.TimetableDay(date: pair.date, timetableCell: LessonModel.createCorrectTimeTable(currentArray: pair.timetableCell))
-                }
-                self?.tableView.reloadData()
-                self?.loader.stopAnimating()
+            self.arrayOfDaysWithLessons = TimetableWeek.convert(response).days.map { pair in
+                return TimetableWeek.TimetableDay(date: pair.date, timetableCell: LessonModel.createCorrectTimeTable(currentArray: pair.timetableCell))
+            }
+            
+            DispatchQueue.main.async() { [weak self] in
+                guard let self = self else { return }
+                self.tableView.reloadData()
+                self.loader.stopAnimating()
+                self.tableView.refreshControl = self.refreshControl
+                self.emptyWeek.isHidden = !self.arrayOfDaysWithLessons.isEmpty
+
+            }
+        }
+    }
+    
+    public func refreshDate(completion: @escaping () -> Void = {}) {
+        let id = (GroupsAndTeacherStorage.shared.currentFilter == .groups) ? GroupsAndTeacherStorage.shared.currentGroupNumber?.ID : GroupsAndTeacherStorage.shared.currentTeachersName?.ID
+        TimetableProvider.shared.loadTimetabe(id: id ?? -1, filter: GroupsAndTeacherStorage.shared.currentFilter, startDate: date, fromCache: false) {
+            response in
+
+            guard let response = response.data else { return }
+            self.arrayOfDaysWithLessons = TimetableWeek.convert(response).days.map { pair in
+                return TimetableWeek.TimetableDay(date: pair.date, timetableCell: LessonModel.createCorrectTimeTable(currentArray: pair.timetableCell))
+            }
+            
+            DispatchQueue.main.async() { [weak self] in
+                guard let self = self else { return }
+                self.tableView.reloadData()
+                self.emptyWeek.isHidden = !self.arrayOfDaysWithLessons.isEmpty
+                completion()
             }
         }
     }
