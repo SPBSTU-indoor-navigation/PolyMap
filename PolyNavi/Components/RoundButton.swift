d077c02780043bc54e1a225ef88199cf39a2f276
Andrei <soprachev@mail.ru>
2021-11-15 19:18:20 +0300

    MapView implement

--- PolyNavi/Components/RoundButton.swift
+++ PolyNavi/Components/RoundButton.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 class RoundButton: UIButton {
+    
+    
+    private lazy var blur: UIVisualEffectView = {
+        $0.layer.masksToBounds = true
+        return $0
+    }(UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterial)))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //Нужно чтоб не баговало масштабирование кнопки при отсутствующем стиле
+        if #available(iOS 15.0, *) {
+            configuration = .plain()
+        }
+        blur.isUserInteractionEnabled = false
+        addSubview(blur)
+        if let imageView = self.imageView{
+            bringSubviewToFront(imageView)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        layer.cornerRadius = bounds.height / 2
-        contentEdgeInsets = UIEdgeInsets(top: 10, left: bounds.height / 3, bottom: 10, right: bounds.height / 3)
+        blur.frame = bounds
+        blur.layer.cornerRadius = bounds.height / 2
+        contentEdgeInsets = UIEdgeInsets(top: 15, left: bounds.height / 3, bottom: 15, right: bounds.height / 3)
     }
 }
