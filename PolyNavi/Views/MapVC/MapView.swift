c7f7623407de1f0cde47551c785f6aad6299a692
Andrei <soprachev@mail.ru>
2021-12-21 20:39:37 +0300

    PolyTech map

--- PolyNavi/Views/MapVC/MapView.swift
+++ PolyNavi/Views/MapVC/MapView.swift
@@ -11,39 +11,28 @@ import MapKit
 class MapView: UIView {
     
     let centerPosition = CLLocationCoordinate2D(latitude: 60.00385, longitude: 30.37539)
-    var venue: Venue?
-    var levels: [Level] = []
-    var currentLevelFeatures = [StylableFeature]()
-    var currentLevelOverlays = [MKOverlay]()
-    var currentLevelAnnotations = [MKAnnotation]()
-    let pointAnnotationViewIdentifier = "PointAnnotationView"
-    let labelAnnotationViewIdentifier = "LabelAnnotationView"
-    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        map.delegate = self
+        mapView.delegate = self
         loadIMDF()
         
         layoutViews()
-        showFeaturesForOrdinal(0)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    lazy var map: MKMapView = {
-        $0.translatesAutoresizingMaskIntoConstraints = false
+    func loadIMDF() {
+        _ = Bundle.main.resourceURL!.appendingPathComponent("IMDFData")
+    }
     
-        
-//        $0.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: MKCoordinateRegion(center: centerPosition, latitudinalMeters: 1000, longitudinalMeters: 1000)), animated: false)
-//
-//        $0.setCameraZoomRange(MKMapView.CameraZoomRange(minCenterCoordinateDistance: 0, maxCenterCoordinateDistance: 5000), animated: false)
-//        $0.setCenter(centerPosition, animated: true)
-        $0.register(PointAnnotationView.self, forAnnotationViewWithReuseIdentifier: pointAnnotationViewIdentifier)
-        $0.register(LabelAnnotationView.self, forAnnotationViewWithReuseIdentifier: labelAnnotationViewIdentifier)
+    lazy var mapView: MKMapView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: MKCoordinateRegion(center: centerPosition, latitudinalMeters: 1000, longitudinalMeters: 1000)), animated: false)
+        $0.setCameraZoomRange(MKMapView.CameraZoomRange(minCenterCoordinateDistance: 0, maxCenterCoordinateDistance: 5000), animated: false)
+        $0.setCenter(centerPosition, animated: true)
         $0.pointOfInterestFilter = .excludingAll
         
         return $0
@@ -51,13 +40,18 @@ class MapView: UIView {
     
     func layoutViews() {
         
-        addSubview(map)
+        addSubview(mapView)
         
         NSLayoutConstraint.activate([
-            map.leadingAnchor.constraint(equalTo: leadingAnchor),
-            map.trailingAnchor.constraint(equalTo: trailingAnchor),
-            map.topAnchor.constraint(equalTo: topAnchor),
-            map.bottomAnchor.constraint(equalTo: bottomAnchor)
+            mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mapView.topAnchor.constraint(equalTo: topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 }
+
+extension MapView: MKMapViewDelegate {
+    
+}
+
