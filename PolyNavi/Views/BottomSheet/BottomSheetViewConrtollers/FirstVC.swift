e747b79ef60b6dc8605b8e987312b12b0849bf56
Andrei <soprachev@mail.ru>
2022-01-31 03:10:56 +0300

    Beautify bottom sheet

--- PolyNavi/Views/BottomSheet/BottomSheetViewConrtollers/FirstVC.swift
+++ PolyNavi/Views/BottomSheet/BottomSheetViewConrtollers/FirstVC.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 
-class FirstVC: UIViewController {
+class FirstVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     private lazy var searchController: UISearchController = {
         $0.obscuresBackgroundDuringPresentation = false
@@ -9,13 +9,47 @@ class FirstVC: UIViewController {
         return $0
     }(UISearchController(searchResultsController: nil))
     
+    private lazy var tableView: UITableView = {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        $0.dataSource = self
+        $0.delegate = self
+        $0.backgroundColor = .clear
+        return $0
+    }(UITableView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-        navigationItem.title = "Search View"
+//        navigationItem.title = "Search View"
         searchController.hidesNavigationBarDuringPresentation = false
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        tableView.reloadData()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.pushViewController(FirstVC(), animated: true)
+    }
 }
--- PolyNavi/Views/BottomSheet/RootBottomSheetViewController.swift
+++ PolyNavi/Views/BottomSheet/RootBottomSheetViewController.swift
@@ -1,46 +1,74 @@
 import UIKit
 
+func expLimit(_ x: CGFloat, _ maxVal: CGFloat) -> CGFloat {
+    return (1 - exp(-x / maxVal)) * maxVal
+}
+
 class RootBottomSheetViewController: UINavigationController {
+    let VELOCITY_LIMIT = 500.0
+    let VELOCITY_SUPER_LIMIT = 2500.0
     
-    enum SizeOfSheet {
-        case fullScreen
-        case partOfScreen
+    
+    enum Size {
+        case big
+        case small
+        case ultraSmall
     }
     
-    private var sizeState: SizeOfSheet {
-        get {
-            let parentFrame = parentVC.view.frame
-            return parentFrame.size.width > 800 ? .partOfScreen : .fullScreen
+    enum State {
+        case min
+        case middle
+        case max
+        
+        var stateBigger: [State] {
+            switch self {
+            case .min: return [.middle, .max]
+            case .middle, .max: return [.max]
+            }
+        }
+        
+        var stateSmaller: [State] {
+            switch self {
+            case .min, .middle: return [.min]
+            case .max: return [.min, .middle]
+            }
         }
     }
     
+    
+    private var state: State = .min
+    private var currentPosition: CGFloat = -1
+    
     private var parentVC: UIViewController!
-    
-    private var yPostion: CGFloat = 0.0
-    
-    private var minYPosition: CGFloat {
-        let parentFrame = parentVC.view.frame
-        let navigationFrame = navigationBar.frame
-        return parentFrame.size.height - navigationFrame.size.height - 20
-    }
-    
-    private var middleYPosition: CGFloat {
-        return parentVC.view.frame.size.height * 0.4
-    }
-    
-    private var maxYPosition: CGFloat {
-        parentVC.view.frame.size.height * 0.2
-    }
-    
     private let searchOverlay = HitAcrossView()
+    var scrollView: UIScrollView?
     
+    private var currentSize: Size {
+        let windowWidth = view.window!.frame.width
+        
+        if windowWidth > 1000 { return .small }
+        if windowWidth > 800 { return .ultraSmall }
+        
+        return .big
+    }
+    
+
     public init(parentVC: UIViewController, rootViewController: UIViewController) {
         self.parentVC = parentVC
         
         super.init(rootViewController: rootViewController)
-        
+    
         let gr = UIPanGestureRecognizer(target: self, action: #selector(panAction(_:)))
-        navigationBar.addGestureRecognizer(gr)
+        
+        rootViewController.view.subviews.forEach({
+            if let scroll = $0 as? UIScrollView {
+                scroll.panGestureRecognizer.require(toFail: gr)
+                scrollView = scroll
+                return
+            }
+        })
+        
+        view.addGestureRecognizer(gr)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -50,80 +78,123 @@ class RootBottomSheetViewController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .secondarySystemGroupedBackground
-        view.layer.cornerRadius = 15
+        let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .systemThickMaterial))
+        blurView.frame = view.frame
+        view.insertSubview(blurView, at: 0)
+        
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 10
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        if yPostion == 0 {
-            yPostion = minYPosition
-        }
+        if currentPosition < 0 { currentPosition = position(for: state) }
         
-        let parentFrame = parentVC.view.frame
         view.frame = CGRect(
-            x: sizeState == .fullScreen ? 0 : 10,
-            y: yPostion,
-            width: sizeState == .fullScreen ? parentFrame.size.width : parentFrame.size.width * 0.45,
-            height: parentFrame.height * 0.8
+            x: currentSize == .big ? 0 : max(view.window!.safeAreaInsets.left, 8),
+            y: currentPosition,
+            width: width(for: currentSize),
+            height: 1000
         )
     }
     
+    
+    func width(for size: Size) -> CGFloat {
+        switch size {
+        case .big:
+            return view.window!.frame.width
+        case .small:
+            return 380
+        case .ultraSmall:
+            return 320
+        }
+    }
+    
+    func position(for state: State) -> CGFloat {
+        let safeArea = view.window!.safeAreaInsets
+        let height = view.window!.frame.height
+        
+        switch state {
+        case .min:
+            return height - safeArea.bottom - 100
+        case .middle:
+            return height - safeArea.bottom - 305
+        case .max:
+            return safeArea.top + 20
+        }
+    }
+    
+    func nextState(velocity: CGFloat) -> State {
+        
+        if velocity > VELOCITY_SUPER_LIMIT { return .min}
+        if velocity < -VELOCITY_SUPER_LIMIT { return .max}
+        
+        var possibleStates: [State] = []
+        
+        if velocity < -VELOCITY_LIMIT {
+            possibleStates = state.stateBigger
+        } else if velocity > VELOCITY_LIMIT {
+            possibleStates = state.stateSmaller
+        } else {
+            possibleStates = [.min, .max, .middle]
+        }
+        
+        var nearestDistance = CGFloat.greatestFiniteMagnitude
+        var nearestState = State.min
+        
+        for state in possibleStates {
+            let distance = abs(position(for: state) - currentPosition)
+            if distance < nearestDistance {
+                nearestState = state
+                nearestDistance = distance
+            }
+        }
+        
+        return nearestState
+    }
+    
+    
+    var startDelta = 0.0
+    
     @objc
     private func panAction(_ sender: UIPanGestureRecognizer) {
+        defer { viewDidLayoutSubviews() }
+        
         let location = sender.location(in: parentVC.view)
-        let viewHeight = parentVC.view.frame.height * 0.8
-        let velocity = sender.velocity(in: navigationBar)
-        let slideFactor = 0.1
-        let minYPos = parentVC.view.frame.height - viewHeight
+        let velocity = sender.velocity(in: view.window!)
+
         switch sender.state {
         case .began:
-            if location.y > minYPos {
-                changeLocation(yPos: location.y)
-            }
-            return
+            startDelta = currentPosition - location.y
+            view.layer.removeAllAnimations()
         case.changed:
-            if location.y > minYPos {
-                changeLocation(yPos: location.y)
+            let minPos = position(for: .min)
+            let maxPos = position(for: .max)
+            let targetPosition = location.y + startDelta
+            
+            if maxPos < targetPosition && targetPosition < minPos {
+                currentPosition = targetPosition
+            } else if targetPosition > minPos {
+                let delta = targetPosition - minPos
+                currentPosition = minPos + expLimit(delta, 50)
+            } else if targetPosition < maxPos {
+                let delta = maxPos - targetPosition
+                currentPosition = maxPos - expLimit(delta, 20)
             }
-            return
+            
         case.ended:
-            if location.y > minYPos {
-                endLocation(yPos: location.y + velocity.y * slideFactor)
-            }
-            return
+            state = nextState(velocity: velocity.y)
+            let delta = abs(currentPosition - position(for: state))
+            currentPosition = position(for: state)
+            
+            let initialSpeed = min(abs(velocity.y / delta / 2), 5)
+            UIView.animate(withDuration: 0.45, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: initialSpeed, options: .curveEaseOut, animations: { [self] in
+                viewDidLayoutSubviews()
+            })
         default:
             return
         }
-    }
-    
-    func changeLocation(yPos: CGFloat) {
-        yPostion = yPos
-        viewDidLayoutSubviews()
-    }
-    
-    func endLocation(yPos: CGFloat) {
-        let animationDuration = CGFloat(0.08)
-        defer {
-            UIView.animate(withDuration: animationDuration, animations: { [weak self] in
-                self?.viewDidLayoutSubviews()
-            })
-        }
-        
-        let padding = CGFloat(15)
-        
-        if yPos >= minYPosition - padding {
-            yPostion = minYPosition
-            return
-        }
-        let t = middleYPosition - padding
-        let b = middleYPosition + padding
-        if yPos <= t || yPos >= b {
-            yPostion = middleYPosition
-            return
-        }
-        
-        yPostion = maxYPosition
     }
 }
