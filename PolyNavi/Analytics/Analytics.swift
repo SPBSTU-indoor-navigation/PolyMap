3e09630c3ecba0ca5546f6e46c0205b4fb921883
Andrei <soprachev@mail.ru>
2022-08-23 00:25:11 +0300

    Add crashes and new api key

--- PolyNavi/Analytics/Analytics.swift
+++ PolyNavi/Analytics/Analytics.swift
@@ -5,4 +5,16 @@
 //  Created by Andrei Soprachev on 21.08.2022.
 //
 
-import Foundation
+import YandexMobileMetrica
+
+
+class Analytics {
+    static let shared = Analytics()
+    
+    func start() {
+        let configuration = YMMYandexMetricaConfiguration.init(apiKey: APP_METRICA_API_KEY)
+        configuration?.locationTracking = false
+        
+        YMMYandexMetrica.activate(with: configuration!)
+    }
+}
